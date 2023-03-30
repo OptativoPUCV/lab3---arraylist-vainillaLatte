c5bd585fa1cb71c6c954c41a60f8603d55486195
Replit user <>
2023-03-30 16:21:41 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -49,7 +49,7 @@ void push(ArrayList * l, void * data, int i){
   }
   if(l->size>i)
   {
-    for(int k = size; i < k; k++)
+    for(int k = l->size; i < k; k++)
       {
         l->data[k+1];
       }
